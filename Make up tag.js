ce08709a7269afef6ac47633a6ad3eae2f427156
Usurupati kamalnath <24B11AI449@adityauniversity.in>
2026-03-23 11:54:08 +0530

    Create MarkupTags component with sample content

---
+++ Make up tag.js
@@ -0,0 +1,14 @@
+import React from "react";
+function MarkupTags() {
+  return (
+    <div className="MarkupTags">
+        <h1>Markup Tags</h1>
+        <p>This is a paragraph.</p>
+        <li> Apple</li>
+        <li> Banana</li>
+        <li> Orange</li>
+        <a href="https://www.google.com">Google</a>
+    </div>
+  );
+}
+export default MarkupTags;
